2f782c3f93159dd5bc1436da956b42fe396c196d
facuayala00 <facundo.ayala@mi.unc.edu.ar>
2022-10-24 04:37:44 -0300

    Tests MLFQ agregados

--- reader.py
+++ reader.py
@@ -4,7 +4,7 @@ import time
 p = Popen(["make", "CPUS=1", "qemu"], stdout=PIPE, stdin=PIPE)
 time.sleep(1)
 
-p.stdin.write("cpubench &; cpubench\n".encode())
+p.stdin.write("iobench &; iobench\n".encode())
 p.stdin.close()
 
 for line in iter(p.stdout.readline, ""):
